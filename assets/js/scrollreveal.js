875f279c135abbbe74deda456e78d2064dcc7ebd
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-09-28 13:54:26 -0300

    att

--- assets/js/scrollreveal.js
+++ assets/js/scrollreveal.js
@@ -5,9 +5,18 @@ const sr = ScrollReveal({
     delay: 200,
 })
 
-sr.reveal(`.hero-title, .footer-top, .hero-text, .section-title`)
-sr.reveal(`.btn-primary, .btn-secondary, .btn-secondary-grow, .about-list, .section-text`, {delay: 400, origin: 'bottom'})
+// show up
+sr.reveal(`.hero-title, .footer-top, .hero-text, .section-title, .cta-title`)
+
+// show bottom
+sr.reveal(`.btn-primary, .btn-secondary, .btn-secondary-grow, .about-list, .section-text, .faq`, {delay: 400, origin: 'bottom'})
+
+// show delay only
 sr.reveal(`.logos__img, .program__card, .pricing__card`, {interval: 100})
+
+// show left
 sr.reveal(`.about-bottom-banner`, {delay: 700, origin: "left"})
-sr.reveal(`.left-text`, {origin: "left"})
-sr.reveal(`.hero-banner, .for-who-list, .why-us-description, .right-text`, {origin: "right"})
+sr.reveal(`.left-text, .contact-content`, {origin: "left"})
+
+// show right
+sr.reveal(`.hero-banner, .for-who-list, .why-us-description, .right-text, .contact-form`, {origin: "right"})
