07dec79b2ab3828276a14142c449188634f59cda
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-09-28 13:18:40 -0300

    created scroll reveal and change colors titlte

--- assets/js/scrollreveal.js
+++ assets/js/scrollreveal.js
@@ -1,1 +1,6 @@
-ScrollReveal().reveal('.container')
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.hero-title', {duration: 1000});
+sr.reveal('.section-title', {duration: 1000});
+sr.reveal('.card-title', {duration: 1000});
+sr.reveal('.cta-title', {duration: 1000});
