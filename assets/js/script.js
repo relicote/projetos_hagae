bea92b90951de226b4fc3dfa613f5e2119d21a37
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-10-05 13:32:54 -0300

    att
    try to set who we are button on mobile view

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,26 @@ window.addEventListener("scroll", function () {
   }
 
 });
+
+// card who-we-are mobile
+
+const modalViews = document.querySelectorAll('.services_modal'),
+    modalBtns = document.querySelectorAll('.services_button'),
+    modalCloses = document.querySelectorAll('.services_modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
